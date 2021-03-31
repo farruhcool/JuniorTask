a46562acf80084191f4350300e43f55c5b3c8c82
f.i.xodjaev <farruhcool1993@gmail.com>
2021-03-31 12:45:03 +0500

    Create base template, other ones extends from

--- travel/views.py
+++ travel/views.py
@@ -6,3 +6,9 @@ def home(request):
     name = "Fred"
 
     return render(request, 'home.html', {'name': name})
+
+
+def about(request):
+    name = "About us"
+
+    return render(request, 'about.html', {'name': name})
