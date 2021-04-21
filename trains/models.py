38579db3b66b80e5e68f4f36930b0de108af8a9e
f.i.xodjaev <farruhcool1993@gmail.com>
2021-04-21 09:16:30 +0500

    Add Train app with model with admin

---
+++ trains/models.py
@@ -0,0 +1,32 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from cities.models import City
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Number of train')
+    travel_time = models.SmallIntegerField(verbose_name='Time of travel')
+    from_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='from_city_set',
+                                  verbose_name='From city')
+    to_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='to_city_set', verbose_name='To city')
+
+    def __str__(self):
+        return f'Train #{self.name} from city {self.from_city}'
+
+    class Meta:
+        verbose_name = "Train"
+        verbose_name_plural = "Trains"
+        ordering = ['travel_time']
+
+    def clean(self):
+        if self.from_city == self.to_city:
+            raise ValidationError('Change to_city')
+        qs = Train.objects.filter(from_city=self.from_city, to_city=self.to_city, travel_time=self.travel_time).exclude(
+            pk=self.pk)
+        if qs.exists():
+            raise ValidationError('Change travel time')
+
+        def save(self, *args, **kwargs):
+            self.clean()
+            super().save(*args, **kwargs)
